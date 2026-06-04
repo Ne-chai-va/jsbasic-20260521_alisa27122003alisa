5e8b08a3b4f1558b599b7e32cda8887c5ff6bb4d
Alisa Nechaeva <ne-chai-va@yandex.ru>
2026-06-04 16:07:38 +0300

    add showSalary function to index.js

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  const filteredUsers = users.filter((item) => item.age <= age);
+  return filteredUsers
+    .map(
+      (item, index, array) =>
+        `${item.name}, ${item.balance}` +
+        (index < array.length - 1 ? "\n" : ""),
+    )
+    .join("");
 }
