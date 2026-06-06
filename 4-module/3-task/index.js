00f2123133548819dc46f28fcfb26f515052fb37
Alisa Nechaeva <ne-chai-va@yandex.ru>
2026-06-06 20:04:56 +0300

    feat: apply dynamic styling to table rows (availability, gender, age filters)

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    if (row.cells[3].dataset.available === "true") {
+      row.classList.add("available");
+    } else if (row.cells[3].dataset.available === "false") {
+      row.classList.add("unavailable");
+    } else if (row.cells[3].dataset.available == null) {
+      row.hidden = true;
+    }
+
+    if (row.cells[2].innerText === "m") {
+      row.classList.add("male");
+    } else if (row.cells[2].innerText === "f") {
+      row.classList.add("female");
+    }
+
+    if (Number(row.cells[1].innerText) < 18) {
+      row.setAttribute("style", "text-decoration: line-through");
+    }
+  }
 }
